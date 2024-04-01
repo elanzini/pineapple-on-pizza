e64935bf4e2de8bb9964c91559c130f0b2929b96
elanzini <edoardo.lanzini@gmail.com>
2024-04-01 10:12:09 +0200

    chore: add versions to requirements and setup

--- setup.py
+++ setup.py
@@ -11,16 +11,16 @@ setup(
     url='https://github.com/elanzini/pineapple_on_pizza',
     packages=find_packages(),
     install_requires=[
-        'nltk',
-        'numpy',
-        'scikit-learn',
-        'matplotlib',
-        'networkx',
-        'transformers',
-        'sentence-transformers',
-        'torch',
-        'openai',
-        'voyageai'
+        'nltk>=3.8.0,<4.0',
+        'numpy>=1.26.0,<2.0',
+        'scikit-learn>=1.2.0,<2.0',
+        'matplotlib>=3.7.0,<4.0',
+        'transformers>=4.38.0,<5.0',
+        'sentence-transformers>=2.6.0,<3.0',
+        'networkx>=3.0,<4.0',
+        'torch>=2.2.0,<3.0',
+        'openai>=1.14.0,<2.0',
+        'voyageai>=0.2.0,<1.0'
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
